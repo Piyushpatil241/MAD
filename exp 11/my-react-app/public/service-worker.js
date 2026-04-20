6a1bf09bb10bb007b0d6379133a9e29538ccaadd
Ryuzaki9052 <piyushsp241@gmail.com>
2026-04-21 00:21:00 +0530

    Finally

---
+++ exp 11/my-react-app/public/service-worker.js
@@ -0,0 +1,9 @@
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+  console.log("Service Worker Installed");
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("Service Worker Activated");
+});
